ba7cceec611cf6d8f4e8e2a5b5989810ad8ab36c
catvajiac <cvajiac@nd.edu>
2017-11-29 03:19:49 -0500

    add config file, some refactoring

--- bot.py
+++ bot.py
@@ -14,7 +14,38 @@ def parse_yaml(filename):
   except:
     return {}
 
-def handle_message(text):
+def parse_config(config):
+  # error checking
+  is_error = False
+  for keyword in ['channels', 'server', 'nick']:
+    if keyword not in config:
+      is_error = True
+      print "please include '{}' in your config file"
+
+  if '-test' in sys.argv and 'test' not in config:
+    is_error = True
+    print "please include a testing channel if you want to test"
+
+  if is_error:
+    print "exiting..."
+    exit(1)
+
+  # parse command line args
+  channels = config['channels']
+  args = sys.argv[1:]
+  while len(args) and args[0].startswith('-') and len(args[0]) > 1:
+    arg = args.pop(0)
+    
+    if arg == '-test':
+      channels = config['test']
+
+  servers = config['server']
+  nicks = config['nick']
+
+  return channels, servers, nicks
+
+
+def handle_message(irc, text, sayings):
   try:
     msg_info = text[1:].split()
     user = msg_info[0].split("!")[0]
@@ -25,8 +56,37 @@ def handle_message(text):
 
   handle_keywords(user, message, channel)
   
-  if message[0] == "$":
-    handle_commands(user, message[1:], channel, sayings)
+  if message.startswith('$'):
+    if message.startswith('$whois'):
+      handle_whois(irc, channel, message, sayings)
+
+    else:
+      handle_commands(user, message[1:], channel, sayings)
+
+def handle_whois(irc, channel, text, sayings):
+  args = text.split()
+  command = args.pop(0)
+
+  # handle name redirection
+  name = args[0]
+  if name in sayings and sayings[name][0] in sayings:
+    key = sayings[name][0]
+  else:
+    key = name
+
+  flag = filter(lambda x: x[0] == '-', args)
+
+  if key not in sayings:
+    irc.send(channel, "I just haven't met them yet")
+    return
+
+  if "-a" in flag:
+    to_send = ["{}, {}".format(name, s) for s in sayings[key]]
+    for msg in to_send:
+      irc.send(channel, msg)
+  else:
+    index = random.randrange(len(sayings[key]))
+    irc.send(channel, "{}, {}".format(name, sayings[key][index]))
 
 def handle_keywords(user, text, channel):
   keywords = ["bork", "sbrk", "fark", "womp"]
@@ -45,28 +105,6 @@ def handle_commands(user, text, channel, sayings):
   args = text.split()
   command = args.pop(0)
 
-  if command == "whois":
-    # handle name redirection
-    name = args[0]
-    if name in sayings and sayings[name][0] in sayings:
-      key = sayings[name][0]
-    else:
-      key = name
-
-    flag = filter(lambda x: x[0] == '-', args)
-
-    if key not in sayings:
-      irc.send(channel, "I just haven't met them yet")
-      return
-
-    if "-a" in flag:
-      to_send = ["{}, {}".format(name, s) for s in sayings[key]]
-      for msg in to_send:
-        irc.send(channel, msg)
-    else:
-      index = random.randrange(len(sayings[key]))
-      irc.send(channel, "{}, {}".format(name, sayings[key][index]))
-
   if command == "motivation" or command == "catjoke":
     name = args[0] if len(args) else user
     index = random.randrange(len(sayings[command]))
@@ -80,21 +118,21 @@ def handle_commands(user, text, channel, sayings):
     irc.send(channel, sayings[command][index].format(source, target))
 
 if __name__ == "__main__":
-  channel = "##sandbox"
-  server = "irc.snoonet.org"
-  nick = "kittykatbot"
+  config = parse_yaml('config.yaml')
+  channels, servers, nicks = parse_config(config)
 
   irc = IRC()
-  irc.connect(server, channel, nick)
+  for i, channel in enumerate(channels):
+    irc.connect(servers[i], channels[i], nicks[i])
 
-  text = irc.recieve()
-
-  while text.find("MODE") == -1: 
     text = irc.recieve()
 
-  irc.join(channel)
-  irc.send("trogdorthedagron", "is this thing on")
+    while text.find("MODE") == -1: 
+      text = irc.recieve()
 
+    irc.join(channel)
+
+  irc.send("trogdorthedagron", "is this thing on")
   sayings = parse_yaml("whois.yaml")
 
   while True:
@@ -104,4 +142,4 @@ if __name__ == "__main__":
       print text.strip()
 
     if "PRIVMSG" in text:
-      handle_message(text) 
+      handle_message(irc, text, sayings) 
